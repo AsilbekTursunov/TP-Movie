743a10415f93372c690b417344fab21ff12e3d71
Asilbek_IMAN <asilbekt84@gmail.com>
2024-03-10 11:22:37 +0500

    new update navigate

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,22 @@
 import { Box, Container, Grid, Stack, Typography, } from '@mui/material'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import SearchBar from '../search-bar/search-bar'
 import { colors } from '../constants/color'
 
 const Navbar = ({ getFilter }) => {
 
+  const navigate = useNavigate() 
+
+  const sendVideo = () =>{ 
+    navigate(`/tp-movie`)
+  }
+
   return (
     <Stack sx={{ backgroundColor: colors.primary }}>
       <Container>
         <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'} py={2}>
-          <Link href="#" to={'/tp-movie/'} underline={'none'} style={{ textDecoration: 'none' }} >
+          <Link href="#" onClick={()=>sendVideo} underline={'none'} style={{ textDecoration: 'none' }} >
             <Typography className="logo-name" style={{ display: 'flex', alignItems: 'center ' }}>
               <img src="http://www.clipartbest.com/cliparts/xig/K4p/xigK4pojT.png" alt="logo" style={{ width: 50, marginRight: '1rem' }} />
               <Typography fontSize={{ xs: '25px',  md: '35px' }} fontFamily={'Cambria'} fontWeight={600} color={colors.textColor}>
